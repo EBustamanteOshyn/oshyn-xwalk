7fe31d9ab13f0af517476852555eda2eda4b43c1
Ocunduri <oscar_cg22@hotmail.com>
2026-03-23 10:06:40 -0500

    Add new field to card component

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -9,8 +9,13 @@ export default function decorate(block) {
     moveInstrumentation(row, li);
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
+      if (div.children.length === 1 && div.querySelector('picture')) {
+        div.className = 'cards-card-image';
+      } else if (div.children.length === 1 && div.querySelector('a') && !div.querySelector('p')) {
+        div.className = 'cards-card-cta';
+      } else {
+        div.className = 'cards-card-body';
+      }
     });
     ul.append(li);
   });
