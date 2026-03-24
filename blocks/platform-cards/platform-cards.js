5b23840f1e90b138af6fe0d5ead0f8dbf1f7a44a
Ocunduri <oscar_cg22@hotmail.com>
2026-03-24 11:50:00 -0500

    Add platform-cards css and js

--- blocks/platform-cards/platform-cards.js
+++ blocks/platform-cards/platform-cards.js
@@ -1,7 +1,39 @@
-export default function decorate(block) {
-  /* change to ul, li */
-  console.log('decorate platform card');
+import { createOptimizedPicture } from '../../scripts/aem.js';
+import { moveInstrumentation } from '../../scripts/scripts.js';
 
-  const children = [...block.children];
-  console.log(children);
+export default function decorate(block) {
+  block.classList.add('tw-card-container');
+  [...block.children].forEach((row) => {
+    const a = document.createElement('a');
+    a.classList.add('tw-card');
+    moveInstrumentation(row, a);
+    while (row.firstElementChild) a.append(row.firstElementChild);
+
+    const [imageCol, bodyCol, ctaCol] = a.children;
+    if (bodyCol && imageCol && bodyCol.children.length > 0) {
+      while (bodyCol.firstElementChild) imageCol.append(bodyCol.firstElementChild);
+    }
+    bodyCol.remove();
+
+    imageCol.querySelector('p')?.classList.add('tw-card-text');
+    
+    if (ctaCol && ctaCol.children.length === 1 && ctaCol.querySelector('a')) {
+      const ctaLink = ctaCol.querySelector('a');
+      if (ctaLink.href) a.href = ctaLink.href;
+      const span = document.createElement('span');
+      if (ctaLink.textContent) span.textContent = ctaLink.textContent;
+      ctaCol.replaceWith(span);
+      span.classList.add('tw-card-link');
+    }
+    row.replaceWith(a);
+
+    a.querySelectorAll('picture > img').forEach((img) => {
+      const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
+      moveInstrumentation(img, optimizedPic.querySelector('img'));
+      optimizedPic.querySelector('img').classList.add('tw-card-image');
+      img.closest('picture').replaceWith(optimizedPic);
+    });
+
+    
+  });
 }
