f687164bee64c3e144fc5a08e6ae600fc72f3d4d
Ocunduri <oscar_cg22@hotmail.com>
2026-03-24 15:01:34 -0500

    Fix lint issues

--- blocks/platform-cards/platform-cards.js
+++ blocks/platform-cards/platform-cards.js
@@ -8,15 +8,12 @@ export default function decorate(block) {
     a.classList.add('tw-card');
     moveInstrumentation(row, a);
     while (row.firstElementChild) a.append(row.firstElementChild);
-
     const [imageCol, bodyCol, ctaCol] = a.children;
     if (bodyCol && imageCol && bodyCol.children.length > 0) {
       while (bodyCol.firstElementChild) imageCol.append(bodyCol.firstElementChild);
     }
     bodyCol.remove();
-
-    imageCol.querySelector('p')?.classList.add('tw-card-text');
-    
+    imageCol.querySelector('p')?.classList.add('tw-card-text');    
     if (ctaCol && ctaCol.children.length === 1 && ctaCol.querySelector('a')) {
       const ctaLink = ctaCol.querySelector('a');
       if (ctaLink.href) a.href = ctaLink.href;
@@ -26,14 +23,11 @@ export default function decorate(block) {
       span.classList.add('tw-card-link');
     }
     row.replaceWith(a);
-
     a.querySelectorAll('picture > img').forEach((img) => {
       const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
       moveInstrumentation(img, optimizedPic.querySelector('img'));
       optimizedPic.querySelector('img').classList.add('tw-card-image');
       img.closest('picture').replaceWith(optimizedPic);
-    });
-
-    
+    });    
   });
 }
